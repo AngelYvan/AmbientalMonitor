1ca8385803ff10a9d7ff83ea08c294e117bb19bc
angel <aychp3000@gmail.com>
2019-10-24 05:28:07 -0500

    Creando screens

--- App.js
+++ App.js
@@ -1,17 +1,53 @@
-import React, { Component } from 'react';
-import { View,Text,Image,ImageBackground } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Image,ImageBackground } from 'react-native';
 
-export default class FlexDimensionsBasics extends Component {
+class HomeScreen extends React.Component {
   render() {
-    let pic={
-      uri: './img/sol.png'};
-    let bc={
-      uri: 'https://image.shutterstock.com/image-illustration/degrade-gradient-background-greenyellow-pale-260nw-1383519863.jpg'};
-    return (
-      <ImageBackground source={bc} style={{width: '100%', height: '100%'}}>
-        <Image source={require('./img/sol.png')} style={{width: 193, height: 110}}/>
+    let fondoHome={uri: 'http://quizupso.pansyequitysa.netdna-cdn.com/q/wp-content/uploads/2017/03/13-11.jpg'};
+    return ( 
+      <ImageBackground source={fondoHome} style={{width: '100%', height: '100%'}}>
+        <View source={fondoHome} style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+          <Text>Home Screen</Text>
+          <Button
+            title="Go to Details"
+            onPress={() => this.props.navigation.navigate('Details')}
+          />
+
+        </View>
       </ImageBackground>
     );
   }
 }
 
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('Details')}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator({
+  Home: HomeScreen,
+  Details: DetailsScreen,
+});
+
+export default createAppContainer(RootStack);
